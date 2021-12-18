3d567cc0b05e4ffb667df8fd2a02bab4e2ec780a
Hershell Koger <hersheykoger@yahoo.com>
2021-12-18 06:37:55 -0500

    Pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
 const reverseString = function(str) {
-
+    let result = str.split("").reverse().join("");
+    return result;
 };
+
+// console.log(reverseString("Hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
