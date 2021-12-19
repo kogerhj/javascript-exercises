08865e97134ab0f3765eec6c1fee9fe549b8661d
Hershell Koger <hersheykoger@yahoo.com>
2021-12-19 14:46:50 -0500

    Complete all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+// let testArray = [1, 2, 3, 4, 5, 6];
 
-};
+
+const removeFromArray = function(...args) {
+    let arr = args[0];
+    return arr.filter(item => !args.includes(item))
+}
+
+// console.log(removeFromArray(testArray, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
