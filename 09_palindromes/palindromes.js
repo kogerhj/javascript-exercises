c8970b9aa85ea032cc228328fcb8097cb5b3122d
Hershell Koger <hersheykoger@yahoo.com>
2022-06-09 14:11:40 -0400

    Pass all 'palindrome' function tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = string
+        .replace(/[^\w\s\']|_/g, "")
+        .toLowerCase()
+        .replace(/\s/g,'')
+        .split("")
+        .join("");
+        
+        let reverseString = string
+        .replace(/[^\w\s\']|_/g, "")
+        .toLowerCase()
+        .replace(/\s/g,'')
+        .split("")
+        .reverse()
+        .join("");
 
+    if(newString == reverseString) return true;
+    else return false;
 };
+
+console.log(palindromes("testing"));
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+(/[^\w\s\']|_/g, "")
