675727f1651eaac5b7f1e84420cebe9edf29e49c
Hershell Koger <hersheykoger@yahoo.com>
2022-06-09 20:06:07 -0400

    Pass all 'fibonacci' function tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function (num) {
+    num.toString();
+    if(num < 0) return "OOPS";
+    let arr = [0, 1];
+    for (let i = 2; i <= num; i++) {
+    arr[i] = arr[i -2] + arr[i - 1];
+}
+return arr.slice(-1)[0];
+}
 // Do not edit below this line
 module.exports = fibonacci;
